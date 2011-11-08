850d5ee171317dac5612f41ec12f7c4ef6b5ce4e
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-11-08 16:31:30 -0500

    Update the has_scope exclusion call to designate the controller action with a symbol rather than string.

--- app/controllers/admin/resource_controller.rb
+++ app/controllers/admin/resource_controller.rb
@@ -4,8 +4,8 @@ class Admin::ResourceController < Admin::ApplicationController
   self.responder = Admin::Responder
 
   # Scopes
-  has_scope :page,  default: 1, only: 'index'
-  has_scope :per_page,   default: 5, only: 'index'
+  has_scope :page, default: 1, :only => :index
+  has_scope :per_page, default: 5, :only => :index
 #  has_scope :sort,  using: [:column, :direction]
 
 end
