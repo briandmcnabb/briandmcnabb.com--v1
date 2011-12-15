c7250030827f40e4125111ae1715e7f92f97710c
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-12-15 18:54:42 -0500

    Refactor connections_controller.

--- app/controllers/admin/connections_controller.rb
+++ app/controllers/admin/connections_controller.rb
@@ -1,10 +1,10 @@
 class Admin::ConnectionsController < Admin::ResourceController
-  actions :index, :create, :destroy
+  actions :index, :destroy
   
   def create
     auth = request.env['omniauth.auth']
     current_user.connections.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'], username: auth['info']['nickname'] )
-    create! { admin_connections_url }
+    redirect_to admin_connections_url
   end
   
   
