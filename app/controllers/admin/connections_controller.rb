6434fe3692758efdeeec34e6e464b9096d2fef77
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-12-15 16:47:56 -0500

    Refactor connections_controller.

--- app/controllers/admin/connections_controller.rb
+++ app/controllers/admin/connections_controller.rb
@@ -1,21 +1,16 @@
-class Admin::ConnectionsController < Admin::ApplicationController
-  def index
-    
-  end
-
-  def create
-    render :text => auth_hash.to_yaml
-  end
+class Admin::ConnectionsController < Admin::ResourceController
+  actions :index, :create, :destroy
   
-  def destroy
-    
+  def create
+    auth = request.env['omniauth.auth']
+    @connection = current_user.connections.create(provider: auth['provider'], uid: auth['uid'], username: auth['info']['nickname'] )
+    create!
   end
   
   
   protected
-
-  def auth_hash
-    request.env['omniauth.auth']
-  end
   
+  def begin_of_association_chain
+    current_user
+  end
 end
