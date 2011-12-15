0c96ead74a0f93e6b2496a5a538648fa58a2ea6f
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-12-15 18:29:16 -0500

    Refactor connections_controller.

--- app/controllers/admin/connections_controller.rb
+++ app/controllers/admin/connections_controller.rb
@@ -4,7 +4,7 @@ class Admin::ConnectionsController < Admin::ResourceController
   def create
     auth = request.env['omniauth.auth']
     current_user.connections.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'], username: auth['info']['nickname'] )
-    redirect_to authentications_url
+    redirect_to connections_url
   end
   
   
