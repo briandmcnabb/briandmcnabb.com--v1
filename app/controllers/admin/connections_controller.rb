f633be8bdd6a2a9d5fe1144a898a2028037ee488
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-12-16 18:37:50 -0500

    Troubleshooting

--- app/controllers/admin/connections_controller.rb
+++ app/controllers/admin/connections_controller.rb
@@ -9,14 +9,8 @@ class Admin::ConnectionsController < Admin::ResourceController
   
   def create
     auth = request.env['omniauth.auth']
-    render :text => auth.to_yaml
-    #auth = request.env['omniauth.auth']
-    #provider = auth['provider']
-    #uid = auth['uid']
-    #username = auth['info']['nickname'] || auth['info']['name']
-    #url = auth['info']['urls']
-    #current_user.connections.find_or_create_by_provider_and_uid(provider, uid, username: username)
-    #redirect_to admin_connections_url
+    current_user.connections.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'], username: profile_url(urls, provider))
+    redirect_to admin_connections_url
   end
   
   
@@ -24,5 +18,9 @@ class Admin::ConnectionsController < Admin::ResourceController
   
   def begin_of_association_chain
     current_user
+  end
+
+  def profile_url(urls, provider)
+    urls.select { |k,v| v if v.include?(provider) }.values[0]
   end
 end
