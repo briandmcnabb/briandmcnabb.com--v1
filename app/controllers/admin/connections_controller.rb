0fa61d6edb29095ad67694a5a362529a541ca78e
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-12-16 17:19:22 -0500

    Troubleshooting

--- app/controllers/admin/connections_controller.rb
+++ app/controllers/admin/connections_controller.rb
@@ -10,13 +10,13 @@ class Admin::ConnectionsController < Admin::ResourceController
   def create
     auth = request.env['omniauth.auth']
     render :text => auth.to_yaml
-    -#auth = request.env['omniauth.auth']
-    -#provider = auth['provider']
-    -#uid = auth['uid']
-    -#username = auth['info']['nickname'] || auth['info']['name']
-    -#url = auth['info']['urls']
-    -#current_user.connections.find_or_create_by_provider_and_uid(provider, uid, username: username)
-    -#redirect_to admin_connections_url
+    #auth = request.env['omniauth.auth']
+    #provider = auth['provider']
+    #uid = auth['uid']
+    #username = auth['info']['nickname'] || auth['info']['name']
+    #url = auth['info']['urls']
+    #current_user.connections.find_or_create_by_provider_and_uid(provider, uid, username: username)
+    #redirect_to admin_connections_url
   end
   
   
