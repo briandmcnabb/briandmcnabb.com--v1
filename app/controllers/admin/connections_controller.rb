6522922919baf411face7bd97e9e5171395a2542
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-12-15 19:07:34 -0500

    Refactor connections_controller and resource index partial.

--- app/controllers/admin/connections_controller.rb
+++ app/controllers/admin/connections_controller.rb
@@ -4,7 +4,7 @@ class Admin::ConnectionsController < Admin::ResourceController
   def create
     auth = request.env['omniauth.auth']
     current_user.connections.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'], username: auth['info']['nickname'] )
-    render nil
+    redirect_to admin_connections_url
   end
   
   
